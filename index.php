a01aebc7c3e6a2069bcfe7fe04f38e50651eaef3
“Allie <allieroll22@ufl.edu>
2019-07-16 15:02:14 -0400

    Edit homepage to display changes

--- index.php
+++ index.php
@@ -1,25 +1,21 @@
-
 <?php get_header(); ?>
 
-
 <div class="container">
-  <!-- Hero Image Section -->
   <div class="row">
-    <div class="twelve columns">
-      <?php dynamic_sidebar('hero-image'); ?>
-    </div>
-  </div>
-
-  <!-- About Text Section -->
-  <div class="row">
-    <div class="nine columns">
-      <?php dynamic_sidebar('about-us'); ?>
-    </div>
-    <div class="three columns">
-      <?php dynamic_sidebar('about-us-image'); ?>
-    </div>
-  </div>
-
-
+  <?php
+    if(have_posts()){
+      while(have_posts()){
+        the_post();?>
+        <div class="one-half column posts">
+          <h2><a href="<?php the_permalink();?>"> <?php the_title();?></a></h2>
+          <?php the_post_thumbnail('medium'); ?>
+          <?php the_excerpt(); ?>
+          <p class="orange"><?php echo "Published: " . get_the_date();?></p>
+          <p class="orange"><?php echo "Article written by: " . get_the_author();?></p>
+        </div>
+    <?php  }//ends while loop
+    }//end if statement
+  ?>
+</div>
 </div>
 <?php get_footer(); ?>
