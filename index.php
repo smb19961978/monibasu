9bf2358e6299b1936ccf83b7d8492198f472923e
“Allie <allieroll22@ufl.edu>
2019-07-16 15:01:00 -0400

    Edit index page to see changes

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
-<?php
-// Silence is golden.
+
+<?php get_header(); ?>
+
+
+<div class="container">
+  <!-- Hero Image Section -->
+  <div class="row">
+    <div class="twelve columns">
+      <?php dynamic_sidebar('hero-image'); ?>
+    </div>
+  </div>
+
+  <!-- About Text Section -->
+  <div class="row">
+    <div class="nine columns">
+      <?php dynamic_sidebar('about-us'); ?>
+    </div>
+    <div class="three columns">
+      <?php dynamic_sidebar('about-us-image'); ?>
+    </div>
+  </div>
+
+
+</div>
+<?php get_footer(); ?>
