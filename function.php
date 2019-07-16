7eb1dcd25d84325a7c146ccd720b5f76ddf5b789
“Allie <allieroll22@ufl.edu>
2019-07-16 14:54:37 -0400

    Edit functions for homepage trial

--- function.php
+++ function.php
@@ -1,13 +1,6 @@
-<?php 
-    add_theme_support('post-thumbnails');
-
-    function blank_widgets_init(){
-        
-        
-        register_sidebar()
-    }
-
-
+<?php
+//Adds featured image to posts
+  add_theme_support('post-thumbnails');
 
 
 //Add Custom menus
@@ -15,12 +8,61 @@
 function custom_menus(){
     register_nav_menus( array(
         'header-menu' => ('Header Menu'),
-        'footer-menu' => ('Footer Menu')    
+        'footer-menu' => ('Footer Menu')
     ));
-    
 }
-
-
 add_action ('init', 'custom_menus');
+
+//Adds widget areas
+function blank_widgets_init(){
+
+  //Header Widget
+  register_sidebar( array(
+    'name'          =>  ('Hero Image'),
+    'id'            =>  'hero-image',
+    'description'   =>  'Hero Image on Home Page',
+    'before_widget' =>  '<div class="hero-image-widget">',
+    'after_widget'  =>  '</div>',
+    'before_title'  =>  '<h3 class="widget-title">',
+    'after_title'   =>  '</h3>'
+  ));
+
+
+  //Hero Image Widget
+  register_sidebar( array(
+    'name'          =>  ('Hero Image'),
+    'id'            =>  'hero-image',
+    'description'   =>  'Hero Image on Home Page',
+    'before_widget' =>  '<div class="hero-image-widget">',
+    'after_widget'  =>  '</div>',
+    'before_title'  =>  '<h3 class="widget-title">',
+    'after_title'   =>  '</h3>'
+  ));
+
+  //About Us Text Block Widget
+  register_sidebar( array(
+    'name'          =>  ('About Us Text Block'),
+    'id'            =>  'about-us',
+    'description'   =>  'Widget area for About Us section',
+    'before_widget' =>  '<div class="about-us">',
+    'after_widget'  =>  '</div>',
+    'before_title'  =>  '<h3 class="widget-title">',
+    'after_title'   =>  '</h3>'
+  ));
+
+  //About Us Image Block Widget
+  register_sidebar( array(
+    'name'          =>  ('About Us Image Block'),
+    'id'            =>  'about-us-image',
+    'description'   =>  'Widget area for About Us image section',
+    'before_widget' =>  '<div class="about-us-image">',
+    'after_widget'  =>  '</div>',
+    'before_title'  =>  '<h3 class="widget-title">',
+    'after_title'   =>  '</h3>'
+  ));
+  add_action('widgets_init','blank_widgets_init');
+
+
+  ?>
 
 ?>
--- page-home.php
+++ page-home.php
@@ -21,20 +21,6 @@
     </div>
   </div>
 
-  <!-- Interior Pages Section -->
-  <div class="row">
-    <div class="one-third column">
-      <?php dynamic_sidebar('bottom-left-home'); ?>
-    </div>
-
-    <div class="one-third column">
-      <?php dynamic_sidebar('bottom-middle-home'); ?>
-    </div>
-
-    <div class="one-third column">
-      <?php dynamic_sidebar('bottom-right-home'); ?>
-    </div>
-  </div>
 
 </div>
 <?php get_footer(); ?>
