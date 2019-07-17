bd7d2f7b311b4192a0fb309376842eef8138ed15
“Allie <allieroll22@ufl.edu>
2019-07-17 13:52:51 -0400

    Edit writing templates

--- page-writing.php
+++ page-writing.php
@@ -4,14 +4,18 @@
   <div class="row">
     <div class="nine columns single-content">
       <?php
-        if(have_posts()){
-          while(have_posts()){
-            the_post();?>
-            <h2><?php the_title(); ?></h2>
-            <?php the_content();
-          }//end while
-        }//ends if
-      ?>
+      if(have_posts()){
+        while(have_posts()){
+          the_post();?>
+            <h2 class="post-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+            <p class="post-date"><?php echo "Published: " . get_the_date();?></p>
+            <p class="post-author"><?php echo "Written by: " . get_the_author();?></p>
+            <?php the_post_thumbnail('medium'); ?>
+            <?php the_excerpt(); ?>
+
+      <?php  }//ends while loop
+      }//end if statement
+    ?>
     </div>
 
     <div class="three columns">
