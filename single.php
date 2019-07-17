187c84a0d8582a534d87ed8f54741c0c55a23800
“Allie <allieroll22@ufl.edu>
2019-07-17 12:16:31 -0400

    Edit and style post single page content

--- single.php
+++ single.php
@@ -2,24 +2,25 @@
 
 <div class="container">
   <div class="row">
-    <div class="nine columns single-content">
+    <div class="seven columns single-content">
     <?php
       if(have_posts()){
         while(have_posts()){
           the_post();?>
           <h2 class="post-title"><?php the_title(); ?></h2>
           <p class="post-date"><?php echo "Published: " . get_the_date(); ?></p>
-          <div class="post-feature"> <?php the_post_thumbnail(); ?>
+          <p><?php echo "This post is written by : " . get_the_author(); ?></p>
+          <div class="post-feature"> <?php the_post_thumbnail('medium'); ?>
           </div>
             <?php the_content(); ?>
 
-          <p><?php echo "This post is written by : " . get_the_author(); ?></p>
+
       <?php  }//end while loop
       } //end if statement
 
     ?>
     </div>
-    <div class="three columns">
+    <div class="five columns">
       <?php get_sidebar(); ?>
     </div>
   </div>
