be365ddf20694404394c0bf2e6841211df6baa7e
“Allie <allieroll22@ufl.edu>
2019-07-18 15:59:20 -0400

    Edit writing page

--- page-writing.php
+++ page-writing.php
@@ -6,7 +6,7 @@
 
   <div class="row">
 
-    <div class="nine columns single-content">
+    <div class="ten columns single-content">
         <?php
           global $post;
           $args = array( 'posts_per_page' => 3 );
@@ -26,7 +26,7 @@
 
 
 
-    <div class="three columns">
+    <div class="one column">
       <?php get_sidebar(); ?>
     </div>
 
