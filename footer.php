dd8b0a827e34d9c518cea09646022b32b7fa4ee4
“Allie <allieroll22@ufl.edu>
2019-07-19 14:46:38 -0400

    Edit footer template

--- footer.php
+++ footer.php
@@ -1,21 +1,20 @@
 <footer>
     <div class="container">
         <div class="row icons">
-            <div>
+        
               <?php dynamic_sidebar('facebook'); ?>
-            </div>
-            <div>
+
+
               <?php dynamic_sidebar('twitter'); ?>
-            </div>
-            <div>
+
+
               <?php dynamic_sidebar('instagram'); ?>
-            </div>
-            <div>
+
               <?php dynamic_sidebar('linkedin'); ?>
-            </div>
+
         </div>
         <div class="copyright">
-              <p>Moni Basu <?php echo date("Y");?> &copy; </p>  
+              <p>Moni Basu <?php echo date("Y");?> &copy; </p>
         </div>
     </div>
 
