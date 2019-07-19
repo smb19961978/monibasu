3c1f37cc39cd8a7c22ca909ea23ff059089f7495
“Allie <allieroll22@ufl.edu>
2019-07-19 14:50:17 -0400

    Justify footer icons

--- footer.php
+++ footer.php
@@ -1,18 +1,20 @@
 <footer>
     <div class="container">
         <div class="row icons">
-        
+          <div class="three columns icons">
               <?php dynamic_sidebar('facebook'); ?>
-
-
+          </div>
+          <div class="three columns icons">
               <?php dynamic_sidebar('twitter'); ?>
-
-
+          </div>
+          <div class="three columns icons">
               <?php dynamic_sidebar('instagram'); ?>
-
+          </div>
+          <div class="three columns icons">
               <?php dynamic_sidebar('linkedin'); ?>
 
         </div>
+      </div>
         <div class="copyright">
               <p>Moni Basu <?php echo date("Y");?> &copy; </p>
         </div>
