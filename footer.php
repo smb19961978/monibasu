a3133dcd88f447cb01ae11a1860960eb5c89e1dc
Bryana <bryananwall@gmail.com>
2019-07-18 23:03:06 -0500

    will display flex social icons

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="container">
-        <div class="row">
+        <div class="row icons">
             <div>
               <?php dynamic_sidebar('facebook'); ?>
             </div>
