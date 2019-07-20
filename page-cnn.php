04c9ca1d630ddd3a15b010b57b00819e3514b075
Bryana <bryananwall@gmail.com>
2019-07-20 12:51:57 -0500

    add featured image to cnn page

--- page-cnn.php
+++ page-cnn.php
@@ -1,5 +1,13 @@
 <?php /*Template Name: CNN Page Template*/ ?>
 <?php get_header(); ?>
+
+<?php 
+    if(has_post_thumbnail()){ ?>
+        <div class="hero-image">
+            <?php the_post_thumbnail(); ?>
+        </div> 
+     <?php } ?> 
+
 <div class="container templates">
 
     <h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
