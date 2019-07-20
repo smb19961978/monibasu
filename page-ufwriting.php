ce07767d901760b801dbd4414b5eab714f12dc17
Bryana <bryananwall@gmail.com>
2019-07-20 14:32:13 -0500

    create a page for uf writing

---
+++ page-ufwriting.php
@@ -0,0 +1,45 @@
+<?php /*Template Name: Writing Page Template*/ ?>
+<?php get_header(); ?>
+
+<?php 
+    if(has_post_thumbnail()){ ?>
+        <div class="hero-image">
+            <?php the_post_thumbnail(); ?>
+        </div> 
+     <?php } ?> 
+
+
+<div class="container templates">
+
+    <h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
+  <div class="row">
+
+    <div class="nine columns single-content">
+        <?php
+          global $post;
+          $args = array( 'posts_per_page' => 3 );
+          $lastposts = get_posts( $args );
+          foreach ( $lastposts as $post ) :
+            setup_postdata( $post ); ?>
+
+
+          <?php the_post_thumbnail('medium'); ?>
+          <h3 class="post-title-template"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+          <p class="post-date-template"><?php echo "Published " . get_the_date(); ?></p>
+          <p class="excerpt"><?php the_excerpt(); ?></p>
+
+        <?php endforeach;?>
+        <?php wp_reset_postdata(); ?>
+
+    </div>
+
+
+
+    <div class="three columns template">
+      <?php get_sidebar(); ?>
+    </div>
+
+  </div>
+</div>
+<?php get_footer(); ?>
