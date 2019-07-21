327594d210cfa946892d9db616a8d950ef018b8f
Bryana <bryananwall@gmail.com>
2019-07-21 16:26:13 -0500

    change styles on archive page

--- archive.php
+++ archive.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
-<div class="container templates">
+<div class="container templates archive">
     <div class="row">
         <div class="nine columns">
     <?php
         if(have_posts()){
             while(have_posts()){
                 the_post();?>
-                <div class="twelve columns">
+                <div class="nine columns">
                 <h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
                 <p><?php the_excerpt(); ?></p>
                 <a href="<?php the_permalink();?>"></a>
