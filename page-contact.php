0809905b818d390710cd7ba5d27adab8609a0e90
Savannah  Greene <savannahgreene@Savannahs-MacBook-Pro.local>
2019-07-21 15:15:33 -0500

    contact page updates

--- page-contact.php
+++ page-contact.php
@@ -9,24 +9,23 @@
      <?php } ?>
 
 <div class="container templates featured-image-container">
-    <h1 class="page-title red"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-
      <div class="row contact-us-section">
 
        <div class="nine columns about">
          <?php dynamic_sidebar('about-us'); ?>
        </div>
 
-       <div class="three columns">
-          <?php
-            if(have_posts()){
-              while(have_posts()){
-                the_post();?>
-                <?php the_content();
-                }//end while
-              }//ends if
-            ?>
-      </div>
+      <div class="three columns contact-heading">
+       <?php
+         if(have_posts()){
+           while(have_posts()){
+             the_post();?>
+             <h2><?php the_title(); ?></h2>
+             <?php the_content();
+           }//end while
+         }//ends if
+       ?>
+     </div>
     </div>
 </div>
 
