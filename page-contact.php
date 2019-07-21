0f07124cef6b2e058ac733c29d176c723de1b09b
Savannah  Greene <savannahgreene@Savannahs-MacBook-Pro.local>
2019-07-21 14:33:50 -0500

    add about us text section for contact page information

--- page-contact.php
+++ page-contact.php
@@ -8,21 +8,25 @@
         </div>
      <?php } ?>
 
-
 <div class="container templates featured-image-container">
     <h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
+     <div class="row contact-us-section">
 
-    <div class="nine columns">
-        <?php
-          if(have_posts()){
-            while(have_posts()){
-              the_post();?>
-              <?php the_content();
-            }//end while
-          }//ends if
-        ?>
-      </div>
+       <div class="nine columns about">
+         <?php dynamic_sidebar('about-us'); ?>
+       </div>
+
+      <div class="three columns">
+          <?php
+            if(have_posts()){
+              while(have_posts()){
+                the_post();?>
+                <?php the_content();
+                }//end while
+              }//ends if
+            ?>
+     </div>
 </div>
 
 
