1481dff1543db21cb1c5882ebb7abd5bfa6e3faa
Bryana <bryananwall@gmail.com>
2019-07-21 16:15:45 -0500

    add container to archive page

--- archive.php
+++ archive.php
@@ -1,15 +1,17 @@
 <?php get_header(); ?>
 
-<div>
+<div class="container">
     <?php
         if(have_posts()){
             while(have_posts()){
                 the_post();?>
+                <div class="twelve columns">
                 <h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
                 <p><?php the_excerpt(); ?></p>
                 <a href="<?php the_permalink();?>"></a>
-            }
-        }
+                </div>
+        <?php    }//ends the while loop
+        }//ends if statement
     
 ?>    
 </div>
