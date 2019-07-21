e90d0955ec55aae259a0d85103bd742ddb5effda
Savannah  Greene <savannahgreene@Savannahs-MacBook-Pro.local>
2019-07-21 14:22:09 -0500

    update contact page code

--- page-contact.php
+++ page-contact.php
@@ -11,6 +11,20 @@
 
 <div class="container templates featured-image-container">
     <h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
+
+    <div class="nine columns">
+        <?php
+          if(have_posts()){
+            while(have_posts()){
+              the_post();?>
+              <h2><?php the_title(); ?></h2>
+              <?php the_content();
+            }//end while
+          }//ends if
+        ?>
+      </div>
 </div>
+
 
 <?php get_footer(); ?>
