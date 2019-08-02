60de3e24d5fc83471f5359685c69fff2dcb7e695
Savannah  Greene <savannahgreene@Savannahs-MacBook-Pro.local>
2019-08-02 17:19:28 -0500

    add links for amazon purchase to home and book page

--- page-chaplain-turners-war.php
+++ page-chaplain-turners-war.php
@@ -15,7 +15,9 @@
   <div class="row">
     <div class="six columns">
       <h1>CHAPLAIN TURNER'S WAR </h1>
-       <button class="purchase-book-page">Purchase</button>
+      <a href="https://www.amazon.com/Chaplain-Turners-War-Moni-Basu-ebook/dp/B007XULHX4/ref=sr_1_1?keywords=chaplain+turners+war&qid=1564783407&s=digital-text&sr=1-1" target="_blank" rel="noopener"><button class="purchase-book-page">Purchase</button></a>
+
+       ><button class="purchase">Purchase</button>
       <p class="chaplain-caption">This is a caption</p>
     </div>
 
