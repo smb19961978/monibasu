1343688a5b393e7bb9b7e9f76abdcca90ca9144a
Savannah  Greene <savannahgreene@Savannahs-MacBook-Pro.local>
2019-08-02 16:57:44 -0500

    Add styles to footer social icons and fix contact page h3

--- footer.php
+++ footer.php
@@ -3,11 +3,6 @@
         <div class="row icons">
           <div class="twelve columns footer">
               <?php if ( function_exists('cn_social_icon') ) echo cn_social_icon(); ?>
-              <?php //dynamic_sidebar('facebook'); ?>
-              <?php //dynamic_sidebar('twitter'); ?>
-              <?php //dynamic_sidebar('instagram'); ?>
-              <?php //dynamic_sidebar('linkedin'); ?>
-
         </div>
       </div>
         <div class="copyright">
