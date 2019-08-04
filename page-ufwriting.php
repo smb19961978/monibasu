d387e679eadd4882a4062ec419d3dcd0a19b1437
Savannah  Greene <savannahgreene@Savannahs-MacBook-Pro.local>
2019-08-04 15:36:32 -0500

    styling and uf page changes

--- page-ufwriting.php
+++ page-ufwriting.php
@@ -19,22 +19,6 @@
         <?php dynamic_sidebar('uf-image'); ?>
         <?php dynamic_sidebar('uf-text'); ?>
 
-        <?php
-          global $post;
-          $args = array( 'posts_per_page' => 3 );
-          $lastposts = get_posts( $args );
-          foreach ( $lastposts as $post ) :
-            setup_postdata( $post ); ?>
-
-
-          <?php the_post_thumbnail('medium'); ?>
-          <h3 class="post-title-template"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-          <p class="post-date-template"><?php echo "Published " . get_the_date(); ?></p>
-          <p class="excerpt"><?php the_excerpt(); ?></p>
-
-        <?php endforeach;?>
-        <?php wp_reset_postdata(); ?>
-
     </div>
 
 
