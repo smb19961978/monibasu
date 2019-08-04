83c2933295c1c6bb24eecaa3a027acb8e67bcdd6
Savannah  Greene <savannahgreene@Savannahs-MacBook-Pro.local>
2019-08-04 11:58:30 -0500

    add comments file

---
+++ comments.php
@@ -0,0 +1,16 @@
+<?php
+
+foreach ($comments as $comment):
+  echo '<div class="comment-avatar">';
+  echo get_avatart ($comment, 32);
+  echo '</div>';
+
+  echo '<div class="comment-author">' . get_comment_author_link() . '</div>';
+
+  echo '<div class="comment-date">' . get_comment_date() . '</div>';
+
+  echo '<div class="comment-text">' . get_comment_text() '</div>';
+
+endforeach;
+
+?>
